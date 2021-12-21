d194d76c267065bb51d9b2e20a819dce85a58774
Jaren Borst <jarenborst@student.rocva.nl>
2021-12-21 17:02:39 +0100

    7.3

---
+++ 7-Working with files/7.3 - html & php/menu.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+    
+    <div class = "menu">
+      <ul>
+        <li> menu-items1</li>
+        <li> menu-items2</li>
+        <li> menu-items3</li>
+     </ul>
+    </div>
+</body>
+</html>
